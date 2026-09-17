1ecfdb83959d7c3529dc5173d337181f1adf1050
svatoshgpt <svatoshgpt@users.noreply.github.com>
2026-09-17 20:44:49 +0800

    fix: leave only the GitHub link in the Links section
    The Links section listed four external resources that this project does not
    run, so they are gone and the repository link remains.
    
    Exported filters also uploaded under a hardcoded title that no longer
    matches the application name.

--- Telegram/SourceFiles/ayu/features/filters/filters_utils.cpp
+++ Telegram/SourceFiles/ayu/features/filters/filters_utils.cpp
@@ -357,7 +357,7 @@ void FilterUtils::publishFilters() {
 
 	QHttpPart titlePart;
 	titlePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"title\""));
-	titlePart.setBody("AyuGram Filters");
+	titlePart.setBody("KomaruGram Filters");
 
 	multiPart->append(contentPart);
 	multiPart->append(syntaxPart);
--- Telegram/SourceFiles/ayu/ui/settings/settings_main.cpp
+++ Telegram/SourceFiles/ayu/ui/settings/settings_main.cpp
@@ -140,48 +140,14 @@ void BuildLinks(SectionBuilder &builder) {
 
 	builder.addSubsectionTitle(tr::ayu_LinksHeader());
 
-	const auto controller = builder.controller();
-
 	builder.addButton({
-		.id = u"ayu/channel"_q,
-		.title = tr::ayu_LinksChannel(),
-		.icon = { &st::menuIconChannel },
-		.label = rpl::single(QString("@ayugram")),
-		.onClick = [=] {
-			controller->showPeerByLink(Window::PeerByLinkInfo{
-				.usernameOrId = QString("ayugram"),
-			});
-		},
-	});
-	builder.addButton({
-		.id = u"ayu/chat"_q,
-		.title = tr::ayu_LinksChats(),
-		.icon = { &st::menuIconChats },
-		.label = rpl::single(QString("@ayugramchat")),
-		.onClick = [=] {
-			controller->showPeerByLink(Window::PeerByLinkInfo{
-				.usernameOrId = QString("ayugramchat"),
-			});
-		},
-	});
-	builder.addButton({
-		.id = u"ayu/crowdin"_q,
-		.title = tr::ayu_LinksTranslate(),
-		.icon = { &st::menuIconTranslate },
-		.label = rpl::single(QString("Crowdin")),
-		.onClick = [=] {
-			QDesktopServices::openUrl(
-				QString("https://translate.ayugram.one"));
-		},
-	});
-	builder.addButton({
-		.id = u"ayu/website"_q,
-		.title = tr::ayu_LinksDocumentation(),
+		.id = u"ayu/github"_q,
+		.title = rpl::single(QString("GitHub")),
 		.icon = { &st::menuIconIpAddress },
-		.label = rpl::single(QString("docs.ayugram.one")),
+		.label = rpl::single(QString("svatoshgpt/komarugram")),
 		.onClick = [=] {
 			QDesktopServices::openUrl(
-				QString("https://docs.ayugram.one"));
+				QString("https://github.com/svatoshgpt/komarugram"));
 		},
 	});
 
